e04646027d548c1e1eade8cfa70b7a46368e7bc5
waxa <waxaman@gmail.com>
2015-11-26 19:54:05 +0100

    fix landscape mobile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
+var setHeader = function() {
+	var aux = jQuery( '.inner-header-mobile' ).height();
+	
+	if ( aux <= 70 ) {
+		var aux = jQuery( '#dropdown-mobile' ).css('margin-top', '70px');
+	}
+}
 
 jQuery( document ).ready( function () {
 
 	setParticles();
+
+	setHeader();
 
 	console.log("particles loaded");
 
